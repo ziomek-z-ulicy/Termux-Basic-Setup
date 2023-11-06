8f8e68def39682c659d73238c94992dd76024a95
ziomek-z-ulicy <109912678+ziomek-z-ulicy@users.noreply.github.com>
2023-11-06 20:19:29 +0100

    Add files via upload

---
+++ Start.sh
@@ -0,0 +1,15 @@
+apt update && apt upgrade -y
+clear
+termux-setup-storage
+apt install git curl wget -y
+clear
+apt install python python2 python3 -y
+clear
+apt install zip unzip -y
+clear
+apt install nano vim -y
+clear
+apt install tor -y
+clear
+apt install figlet -y
+figlet Done!

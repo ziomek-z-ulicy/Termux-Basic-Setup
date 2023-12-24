8dee4bb4811cdc36e56d4fd11a6f09888ec97bc1
ziomek-z-ulicy <109912678+ziomek-z-ulicy@users.noreply.github.com>
2023-12-24 13:40:29 +0100

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -2,28 +2,38 @@ clear
 echo You you will need +-1gb of space [press CTRL + C to Abort]
 echo Wait to continue
 sleep 3
+clear
+[?] Updating Termux
 pkg update && pkg upgrade -y
 apt update && apt upgrade -y
 apt full-upgrade -y
+clear
+[?] Giving storage permission to Termux
 termux-setup-storage
+sleep 3
+clear
+[?] Installing usefull packages
+sleep 1
 apt install git curl wget -y
-apt install python python2 python3 -y
 apt install zip unzip -y
 apt install nano vim -y
 apt install tor -y
 apt install figlet -y
-pkg install root-repo -y
-pkg install x11-repo -y
 pkg install termux-api -y
 apt install nmap -y
 apt install core-ultis -y
 apt install tsu -y
 clear
+[?] installing Python (300mb space need)
+sleep 1
+apt install python python2 python3 -y
+clear
+[?] Installing more repo sources
+sleep 1
+pkg install root-repo -y
+pkg install x11-repo -y
+clear
 figlet Done!
 sleep 2
 cd $HOME
-rm -rf Termux-Basic-Setup
-clear
-cd $HOME
-rm -rf Termux-Basic-Setup
-clear
+echo [rm -rf termux-basic-setup] to remove script
